69eb9143c2246b9ccaa564ceeae6c44823240ea8
yu.xia <404638507@qq.com>
2020-06-14 22:22:21 +0800

    Problems283 移动零

---
+++ Week01/MoveZeroes.java
@@ -0,0 +1,59 @@
+package Week01;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MoveZeroes {
+
+    /**
+     * v1 暴力求解，双重循环遍历交换
+     *
+     * @param nums
+     */
+    public void moveZeroesV1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        int temp = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * V2 快慢指针交换
+     *
+     * @param nums
+     */
+    public void moveZeroesV2(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+
+}
